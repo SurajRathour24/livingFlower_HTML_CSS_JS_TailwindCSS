802dc42f508b5d111e80d3da728237e9223d0383
Suraj Rathour <surajrathour0124@gmail.com>
2023-09-04 14:54:20 +0530

    banner

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {
+      colors:{
+        'lf-primary': '#1a6c7a',
+      }
+    },
+  },
+  plugins: [],
+}
+
